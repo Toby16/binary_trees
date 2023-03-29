99d7b822fad36bb2085c8d3c31bf3500501bd237
Toby16 <bolajibaasit14@gmail.com>
2023-03-29 19:13:39 -0400

    Report: test file for '1-binary_tree_insert_left.c' | included excta code to test and improve '1-binary_tree_insert_left.c'

--- tests/1-main.c
+++ tests/1-main.c
@@ -16,6 +16,7 @@ int main(void)
 	printf("\n");
 	binary_tree_insert_left(root->right, 128);
 	binary_tree_insert_left(root, 54);
+	binary_tree_insert_left(root, 2000);
 	binary_tree_print(root);
 	return (0);
 }
