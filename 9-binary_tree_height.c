9bb473e7e0e572867bcdb8e723eaf2f26f63317b
Toby16 <bolajibaasit14@gmail.com>
2023-03-30 11:01:10 -0400

    Height | Report: code refactored to match checker

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,15 +18,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+       	right_h	= tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left_h > right_h ? left_h : right_h);
 
-	if (left_h > right_h)
-	{
-		return (left_h + 1);
-	}
-	else
-	{
-		return (right_h + 1);
-	}
 }
